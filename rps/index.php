3604a44409bab7a0c20726b05402b3415488a852
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2020-10-10 16:36:06 +0600

    title edit

--- rps/index.php
+++ rps/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Charles Severance - Broken Rock Paper Scissors</title>
+<title>Md. Hasanul Bari - Broken Rock Paper Scissors</title>
 <?php require_once "bootstrap.php"; ?>
 </head>
 <body>
@@ -14,7 +14,7 @@ partially done and serves only as a starting point for the assignment.
 <a href="login.php">Please Log In</a>
 </p>
 <p>
-Attempt to go to 
+Attempt to go to
 <a href="game.php">game.php</a> without logging in - it should fail with an error message.
 <p>
 <a href="http://www.wa4e.com/code/rps.zip"
@@ -22,4 +22,3 @@ Attempt to go to
 </p>
 </div>
 </body>
-
