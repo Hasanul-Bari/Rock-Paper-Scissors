c95a2f027ded2e92b06d430201d565dfb225127a
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2020-10-10 16:42:25 +0600

    title edit

--- rps/index.php
+++ rps/index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Md. Hasanul Bari - Broken Rock Paper Scissors</title>
+<title>Md. Hasanul Bari - Rock Paper Scissors</title>
 <?php require_once "bootstrap.php"; ?>
 </head>
 <body>
   <div class="container">
-    <h1>Welcome to Broken Rock Paper Scissors</h1>
+    <h1>Welcome to Rock Paper Scissors</h1>
 
     <p>
     <a href="login.php">Please Log In</a>
